69a2f975bd59273da9372e3c8d929feced033f12
Laise <laise94@hotmail.com>
2022-02-18 22:16:55 -0300

    adicionando lista planetas e nomes requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
-
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+// https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-key.md
 class SolarSystem extends React.Component {
   render() {
+    const planetlist = Planets.map(({ name, image }) => (
+      <PlanetCard
+        key={ name }
+        planetName={ name }
+        planetImage={ image }
+      />
+    ));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div>{ planetlist }</div>
       </div>
     );
   }
